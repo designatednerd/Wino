64bc0a72d1faf6bce4525962810f1e7f82ef1370
Ellen Shapiro <designatednerd@gmail.com>
2015-07-16 17:02:56 -0700

    getting input stuff actually working.

--- app/src/androidTest/java/com/designatednerd/wino/ui/WineTastingDetailUITests.java
+++ app/src/androidTest/java/com/designatednerd/wino/ui/WineTastingDetailUITests.java
@@ -50,8 +50,42 @@ public class WineTastingDetailUITests {
                 .nukeAllSharedPreferences();
     }
 
+    /******************
+     * HELPER METHODS *
+     ******************/
+
     private Context getContext() {
         return wineTastingActivity.getActivity();
+    }
+
+    private void goToDetailsPage() {
+        //Tap the add button on the main page.
+        onView(withId(R.id.add_tasting_button))
+                .perform(click());
+    }
+
+    private void enterAndSaveWineInfo(String aWineName,
+                                      String aVineyardName,
+                                      String aVarietal,
+                                      int aRating) {
+        enterTextIntoViewWithID(aVineyardName, R.id.tasting_detail_vineyard_name_edittext);
+        enterTextIntoViewWithID(aVarietal, R.id.tasting_detail_varietal_edittext);
+        enterTextIntoViewWithID(aWineName, R.id.tasting_detail_wine_name_edittext);
+
+        //NOTE: This would do the same thing with stock edit texts, but doesn't work with
+        //TextInputLayout due to this bug: https://code.google.com/p/android/issues/detail?id=178182
+//        enterTextIntoViewWithHint(aVineyardName, R.string.vineyard_name);
+//        enterTextIntoViewWithHint(aVarietal, R.string.wine_varietal);
+//        enterTextIntoViewWithHint(aWineName, R.string.wine_name);
+
+        tapViewWithID(R.id.tasting_detail_rating_spinner);
+
+        String[] ratings = getContext().getResources().getStringArray(R.array.ratings_array);
+        String desiredRating = ratings[aRating];
+        tapViewWithText(desiredRating);
+
+        //Save the tasting
+        tapViewWithText(R.string.save_tasting_title);
     }
 
     /*****************
@@ -59,29 +93,19 @@ public class WineTastingDetailUITests {
      *****************/
 
     @Test
-//    public void savingDataInTheDetailShowsUpInTheRecyclerView() {
     public void savingDataInTheDetailIsPersistedToUserDefaults() {
-        onView(withId(R.id.add_tasting_button))
-                .perform(click());
+        goToDetailsPage();
 
+        //Enter the text into the
         String testWineName = "Test wine name";
         String testVarietal = "Cabernet Sauvignon";
         String testVineyard = "Rodney Strong";
+        int testRating = 3;
 
-        enterTextIntoViewWithID(testVineyard, R.id.tasting_detail_vineyard_name_edittext);
-        enterTextIntoViewWithID(testVarietal, R.id.tasting_detail_varietal_edittext);
-        enterTextIntoViewWithID(testWineName, R.id.tasting_detail_wine_name_edittext);
-
-        //Select a 3 star rating
-        tapViewWithID(R.id.tasting_detail_rating_spinner);
-        tapViewWithText("Three");
-
-        //Save the tasting
-        tapViewWithText(R.string.save_tasting_title);
-
-        //Back out to the main
-//        pressBack();
-
+        enterAndSaveWineInfo(testWineName,
+                testVineyard,
+                testVarietal,
+                testRating);
 
         String expectedWineName = "Test wine name (Cabernet Sauvignon, Rodney Strong)";
 
@@ -96,8 +120,12 @@ public class WineTastingDetailUITests {
         assertEquals(savedTasting.vineyardName, testVineyard);
         assertEquals(savedTasting.wineName, testWineName);
         assertEquals(savedTasting.wineVarietal, testVarietal);
+        assertEquals(savedTasting.rating, testRating);
         assertEquals(savedTasting.getFullWineName(), expectedWineName);
 
+        //back out to the main view for the next test.
+        pressBack();
+    }
         //TODO: Fix this so it actually works
 //        onRecyclerItemView(R.id.row_tasting,
 //                R.id.row_tasting_wine_name_textview,
